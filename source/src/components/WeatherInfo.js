6ec19b03a56b30ccbbf56f427e68da8accbf776d
Oleg Roganin <94141484+olegroganin@users.noreply.github.com>
2022-01-15 18:25:39 +0200

    Update WeatherInfo.js

--- source/src/components/WeatherInfo.js
+++ source/src/components/WeatherInfo.js
@@ -4,32 +4,32 @@ import { CSSTransition } from 'react-transition-group';
 
 
 function WeatherInfo ({city, temp, conditions, windSpeed, humidity, setTemp, setConditions, setWindSpeed, setHumidity}) {
-	useEffect(() => {
-		if(city === '') {
-			setTemp('')
-			setConditions('')
-			setWindSpeed('')
-			setHumidity('')
+   useEffect(() => {
+      if(city === '') {
+         setTemp('')
+	 setConditions('')
+	 setWindSpeed('')
+	 setHumidity('')
       }
-	})
+   })
 
    return (
-		<>
-			<CSSTransition
-			   in={temp, conditions, humidity, windSpeed}
-			   timeout={1000}
-			   classNames="weatherapp-info-transition"
-			   unmountOnExit
-			>
-			   <div className='weatherapp-info'>
-			      <p>{temp}</p>
-			      <p>{conditions}</p>
-			      <p>{humidity}</p>
-			      <p>{windSpeed}</p>
-			   </div>
-			</CSSTransition>
-		</>
-	)
+      <>
+         <CSSTransition
+            in={temp, conditions, humidity, windSpeed}
+	    timeout={1000}
+	    classNames="weatherapp-info-transition"
+	    unmountOnExit
+	 >
+	    <div className='weatherapp-info'>
+               <p>{temp}</p>
+	       <p>{conditions}</p>
+	       <p>{humidity}</p>
+	       <p>{windSpeed}</p>
+            </div>
+         </CSSTransition>
+      </>
+   )
 }
 
 export default WeatherInfo;
